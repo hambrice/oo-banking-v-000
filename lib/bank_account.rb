b4385a1e53c8f73660d94d0ac49aadcd2b47258b
Brad Hambrice <hambrice@uab.edu>
2018-01-10 04:55:04 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,21 @@
 class BankAccount
 attr_reader :balance, :status, :name
 
-def initialize(name)
-  @name = name
-  @balance = 1000
-  @status = "open"
-end
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+   @balance = @balance + amount
+  end
+  
+  def display_balance
+    @balance 
+  end
+  
+  def valid?
+   @status == "open" && @balance > 0 
+ end
 end
