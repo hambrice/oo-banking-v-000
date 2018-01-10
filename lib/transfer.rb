8f13eda1cdff6d29ca0b24b4d3f28e90a377137b
Brad Hambrice <hambrice@uab.edu>
2018-01-10 05:15:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ class Transfer
    def reverse_transfer
      if @status == "complete"
        @receiver.balance = @receiver.balance - @amount
-       @sender.balance = @sender.balance +@amount
+       @sender.balance = @sender.balance + @amount
        @status = "reversed"
      end
    end
